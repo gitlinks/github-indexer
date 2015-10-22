54133603bbe5c684b68348a156ec0bc48c2b9754
brunnoattorre <brattorre@gmail.com>
2015-10-22 15:55:06 -0400

    Fixing routing protocol

--- src/main/scala/actors/Master.scala
+++ src/main/scala/actors/Master.scala
@@ -1,11 +1,11 @@
 package actors
 
-import akka.routing.RoundRobinRouter
+import akka.routing._
 
 import sys.process._
 import java.net.URL
 import java.io.File
-import akka.actor.{Props, Actor}
+import akka.actor.{ActorRef, Props, Actor}
 
 /**
  * Created by brunnoattorre1 on 10/22/15.
@@ -15,10 +15,10 @@ class Master extends Actor{
 
    def receive: Receive ={
      case Start =>
-       val workerRouter = context.actorOf(
-         Props[Worker].withRouter(RoundRobinRouter(23)), name = "workerRouter")
+       val router: ActorRef =
+         context.actorOf(RoundRobinPool(23).props(Props[Worker]), "workerRouter")
        for( i <- 0 to 23){
-         workerRouter ! Work(i)
+         router ! Work(i)
        }
    }
 }
--- src/main/scala/actors/Worker.scala
+++ src/main/scala/actors/Worker.scala
@@ -4,7 +4,7 @@ import sys.process._
 import java.io.File
 import java.net.URL
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.{Calendar, Date}
 
 import akka.actor.Actor
 
@@ -14,7 +14,9 @@ import akka.actor.Actor
 class Worker extends Actor {
 
   def downloadAndParse(i: Int): Seq[String] = {
-    val currentDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+    val calendar = Calendar.getInstance()
+    calendar.add(Calendar.DATE, -1)
+    val currentDate = new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime)
     new URL("http://data.githubarchive.org/"+currentDate+ "-"+i+".json.gz") #> new File("/tmp/output"+i+".gzip") !!;
     return List()
   }
